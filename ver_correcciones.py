c5827a7eb4ecf4149fe4452a1af95bfb031dbce9
iKuneSocom <kune.socom@gmail.com>
2025-05-23 17:26:09 +0200

    .

--- ver_correcciones.py
+++ ver_correcciones.py
@@ -1,6 +1,8 @@
+import os
 import sqlite3
 
-conn = sqlite3.connect('stats.db')
+db_path = os.environ.get("DB_PATH", "stats.db")
+conn = sqlite3.connect(db_path)
 cur = conn.cursor()
 for row in cur.execute('SELECT * FROM correcciones ORDER BY fecha DESC'):
     print(row)
